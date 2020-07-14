4c092c0fd1b2e2ccdfb3ee9eac35367037d3fede
Crystal Young <crystal.young@codeimmersives.com>
2020-07-14 15:08:36 -0400

    add

--- get-row.js
+++ get-row.js
@@ -3,6 +3,17 @@
 // The 2D array we'll need to reference to return the right data.
 // Check it out to see its content and shape!
 const pokemon = require('./pokemon.js');
+const { reduceRight } = require('./pokemon.js');
+
+
+const getRow = function(namePokemon) {
+  for ( const item of pokemon) {
+    if ( item[0] === namePokemon) {
+      return item
+    }
+  }
+      return null;
+}
 
 
 
