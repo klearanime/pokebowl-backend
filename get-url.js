5e274db8bd7b6db1b8d688bb14eb8f40ef862ff6
Crystal Young <crystal.young@codeimmersives.com>
2020-07-14 15:26:41 -0400

    add

--- get-url.js
+++ get-url.js
@@ -1,12 +1,12 @@
 // Your code here.
 // Note that you do NOT need the data for this function!
 
-const getUrl = function(address) {
- // for ( const item of address) {
-   if ( address) {
-      return address[1]
-    }
-      return null;
+const getUrl = function (address) {
+  // for ( const item of address) {
+  if (address) {
+    return address[1]
+  }
+  return null;
 }
 console.log()
 
